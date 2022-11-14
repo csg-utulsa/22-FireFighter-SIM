3b8028e34acc753fbce67a96657e5e4a337eb356
aschia <ajs1173@utulsa.edu>
2022-11-14 12:01:15 -0600

    ui changes, pulse effect

--- VR-FireFighter/Assets/Scripts/UI_Pulse.cs
+++ VR-FireFighter/Assets/Scripts/UI_Pulse.cs
@@ -1,23 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UI_Pulse : MonoBehaviour
 {
     // vars
-    [Header("How")]
+    [Header("Color Properties")]
     public Color[] colorCycle;
-    public float timeBetweenColors = 2f;
+    public float timeBetweenColors = 1f;
+
+
+    float colorTime = 0;
+    int colorState = 0;
+
+    float timeBetween_stored = 1f;
+
+    TextMeshProUGUI text;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        text = GetComponent<TextMeshProUGUI>();
+        timeBetween_stored = timeBetweenColors;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        text.color = Color.Lerp(text.color, colorCycle[colorState], colorTime / timeBetweenColors);
+        colorTime += Time.deltaTime;
+        if (text.color == colorCycle[colorState] || colorTime >= timeBetweenColors) {
+            text.color = colorCycle[colorState];
+
+            colorState++;
+            colorTime = 0;
+
+            if (colorState >= colorCycle.Length) {
+                colorState = 0;
+            }
+        }
+    }
+
+    public void ResetColor() {
+        colorState = 0;
+        text.color = colorCycle[colorState];
+        timeBetweenColors = timeBetween_stored;
     }
 }
